cb44b099004d32800a036b7b3894eb3310acee6e
wisteria <abdulrehmantheking007@gmail.com>
2026-09-11 11:59:29 +0500

    Implemented cyclic row decomposition for Program 1 to improve load balance

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -29,13 +29,23 @@ extern void mandelbrotSerial(
 // Thread entrypoint.
 void workerThreadStart(WorkerArgs * const args) {
 
-    // TODO FOR CS149 STUDENTS: Implement the body of the worker
-    // thread here. Each thread should make a call to mandelbrotSerial()
-    // to compute a part of the output image.  For example, in a
-    // program that uses two threads, thread 0 could compute the top
-    // half of the image and thread 1 could compute the bottom half.
-
-    printf("Hello world from thread %d\n", args->threadId);
+    for (int row = args->threadId;
+         row < (int)args->height;
+         row += args->numThreads)
+    {
+        mandelbrotSerial(
+            args->x0,
+            args->y0,
+            args->x1,
+            args->y1,
+            args->width,
+            args->height,
+            row,
+            1,
+            args->maxIterations,
+            args->output
+        );
+    }
 }
 
 //
